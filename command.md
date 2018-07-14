123c651530b5f3ae9d8470444399187e9dbe97f1
dnguyen1196 <ducnguyenmanh96@gmail.com>
2018-07-14 13:55:24 -0400

    add option for dimensionality

--- command.md
+++ command.md
@@ -1,3 +1,5 @@
+# command to learn word embeddings
+
 
 # Command to align embeddings
 python align_word_embeddings.py -e ./embeddings_results/2016_pmi_pair_diag/2016_pair_pmi_181.txt -p /home/mnguye16/PMI_2016/wordPairPMI_2016wordIDs.pickle  -o ./aligned_embeddings/2016_embeddings_diag.txt
--- main.py
+++ main.py
@@ -6,26 +6,31 @@ import pickle
 import argparse
 
 
-def do_synthetic_embeddings(num_words, order, output, diag):
+def do_synthetic_embeddings(num_words, order, output, diag, D):
     pmi_tensor = PMI_tensor()
     pmi_tensor.synthesize_fake_PMI(num_words, order)
+    if D is None:
+        D = 50
+
     if diag:
-        factorizer = SSVI_Embedding_Diag(pmi_tensor)
+        factorizer = SSVI_Embedding_Diag(pmi_tensor, D)
     else:
-        factorizer = SSVI_Embedding_full(pmi_tensor)
+        factorizer = SSVI_Embedding_full(pmi_tensor, D)
     factorizer.produce_embeddings(output)
 
-def do_embeddings_pmi(pickedfile, output, diag):
+def do_embeddings_pmi(pickedfile, output, diag, D):
     print ("Loading tensor...")
     with open(pickedfile, "rb") as f:
         pmi_tensor = pickle.load(f, encoding="latin1")
         num_words  = pmi_tensor.num_words
         order      = pmi_tensor.order
         print("Computing embeddings for ", num_words, " words from ", order, "th pmi tensor ... ")
+        if D is not None:
+            D = 50
         if diag:
-            factorizer = SSVI_Embedding_Diag(pmi_tensor)
+            factorizer = SSVI_Embedding_Diag(pmi_tensor, D)
         else:
-            factorizer = SSVI_Embedding_full(pmi_tensor)
+            factorizer = SSVI_Embedding_full(pmi_tensor, D)
         factorizer.produce_embeddings(output)
 
 parser = argparse.ArgumentParser(description='SSVI word embeddings')
@@ -38,10 +43,12 @@ parser.add_argument("-ord", "--order", type=int, help="order of PMI tensor")
 group.add_argument("-p", "--pfile", type=str, help="Picked PMI object file")
 parser.add_argument("-o", "--output", type=str, help="Output file")
 parser.add_argument("--diag", action="store_true")
+parser.add_argument("-d", "--dimension", type=int)
+
 args = parser.parse_args()
 
 if args.numwords is not None:
-    do_synthetic_embeddings(args.numwords, args.order, args.output, args.diag)
+    do_synthetic_embeddings(args.numwords, args.order, args.output, args.diag, args.dimension)
 if args.pfile is not None:
-    do_embeddings_pmi(args.pfile, args.output, args.diag)
+    do_embeddings_pmi(args.pfile, args.output, args.diag, args.dimension)
 
